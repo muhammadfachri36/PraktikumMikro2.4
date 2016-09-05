7efaa264d86e2941af6536a8decf20e64bf3afda
muhammadfachri36 <mamadfahri4@gmail.com>
2016-09-05 15:48:46 +0700

    Add files via upload

---
+++ Praktikum2.4.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A0, INPUT);
+  digitalWrite(A0, HIGH);
+  pinMode(12, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(A0)==LOW){
+      digitalWrite(12, HIGH);
+    }
+    else{
+         digitalWrite(12, LOW);
+    }
+}
